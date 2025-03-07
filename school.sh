c5da5da97e5d93c7b4c65bb195188de3a4c56230
Aryan Shah <aashah26@wisc.edu>
2025-03-06 20:17:18 -0600

    added pipline to school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,3 @@
-#!/bin/bash
+#!/bin/babsh
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f7 | { sum=0; count=0; while read val; do sum=$((sum + val)); count=$((count + 1)); done; echo "Average TotalAssessedValue: $((sum / count))"; }
